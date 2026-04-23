523696da0536ab99a1ebec291f58bfe63de0abb6
ramdhannassyirahh <ramdhannassyirahh@gmail.com>
2026-04-23 22:09:09 +0700

    Fix Nuxt Config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,16 +4,11 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  runtimeConfig: {
-    public: {
-      apiBase:
-        process.env.API_BASE_URL || "https://www.themealdb.com/api/json/v1/1/",
-    },
-  },
-  css: ["~/assets/css/main.css"],
+  css: ["./app/assets/css/main.css"],
   vite: {
     plugins: [tailwindcss()],
   },
+
   modules: ["@nuxt/image", "@nuxt/icon", "@nuxt/fonts", "@pinia/nuxt"],
   components: [
     {
@@ -21,5 +16,11 @@ export default defineNuxtConfig({
       pathPrefix: false,
     },
   ],
- 
+
+  runtimeConfig: {
+    public: {
+      apiBase:
+        process.env.API_BASE_URL || "https://www.themealdb.com/api/json/v1/1/",
+    },
+  },
 });
